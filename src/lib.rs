fcd3b4f663a77413bd470484757be646452c1b78
ndr3www <164680506+ndr3www@users.noreply.github.com>
2024-04-13 14:45:33 +0200

    add subcommands, appropriate tests and documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,31 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
-pub const APP_NAME: &str = "rustii";
+pub const APP_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
 pub struct Cli {
-    #[arg(long)]
-    render: String,
+    #[command(subcommand)]
+    command: Commands
+}
 
-    #[arg(long)]
-    output: String
+#[derive(Subcommand)]
+enum Commands {
+    /// Render specified media file to ASCII art
+    Render {
+        /// Image or video to be rendered
+        input_file_path: Option<String>,
+        /// ASCII art file path
+        output_file_path: Option<String>,
+        /// Scale of the final ASCII art, default is 1.0
+        scale: Option<f32>
+    },
+    /// Play specified ASCII art in terminal
+    Play {
+        /// ASCII image or video to be played
+        input_file_path: Option<String>
+    }
 }
 
 #[cfg(test)]
@@ -18,6 +34,27 @@ mod tests {
 
     #[test]
     fn case_render() {
-        Cli::parse_from(vec![APP_NAME, "--render", "image.png", "--output", "image.txt"]);
+        let cli = Cli::parse_from([APP_NAME, "render", "image.png", "image.txt", "1.0"]);
+        
+        match &cli.command {
+            Commands::Render { input_file_path, output_file_path, scale } => {
+                assert_eq!(input_file_path, &Some("image.png".to_string()));
+                assert_eq!(output_file_path, &Some("image.txt".to_string()));
+                assert_eq!(scale, &Some(1.0));
+            },
+            _ => ()
+        };
+    }
+
+    #[test]
+    fn case_play() {
+        let cli = Cli::parse_from([APP_NAME, "play", "image.txt"]);
+        
+        match &cli.command {
+            Commands::Play { input_file_path } => {
+                assert_eq!(input_file_path, &Some("image.txt".to_string()));
+            },
+            _ => ()
+        };
     }
 }
