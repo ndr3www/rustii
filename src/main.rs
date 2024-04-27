16848b9e7a3e06a86e7c22f1cacb2d4fbdd5816a
Lukas Eschbacher <eschbacher.lukas@gmail.com>
2024-04-27 20:45:10 +0200

    simplify error handling

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,15 @@ fn main() {
 
     match cli.get_command() {
         Commands::Render { input_file_path, output, scale, contrast } => {
-            match render(input_file_path, output, scale, contrast) {
-                Ok(_) => (),
-                Err(e) => handle_error(e, 1)
-            };
+            if let Err(e) = render(input_file_path, output, scale, contrast) {
+                handle_error(&e, 1);
+            }
         },
 
         Commands::Open { input_file_path } => {
-            match open(input_file_path) {
-                Ok(_) => (),
-                Err(e) => handle_error(e, 2)
-            };
+            if let Err(e) = open(input_file_path) {
+                handle_error(&e, 2);
+            }
         }
     };
 }
