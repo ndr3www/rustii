29a93ca140970e0f68dc5e08f041a4e6f6faf525
ndr3www <164680506+ndr3www@users.noreply.github.com>
2024-04-27 14:42:47 +0200

    forgot to change output file name in previous commit

--- src/lib.rs
+++ src/lib.rs
@@ -238,7 +238,7 @@ mod tests {
 
     #[test]
     fn case_render() {
-        let cli = Cli::parse_from([APP_NAME, "render", "./tests/image.jpg", "-o", "./tests/img.txt", "-s", "0.65", "0.25", "-c", "20"]);
+        let cli = Cli::parse_from([APP_NAME, "render", "./tests/image.jpg", "-o", "./tests/ascii.txt", "-s", "0.65", "0.25", "-c", "20"]);
         
         match cli.get_command() {
             Commands::Render { input_file_path, output, scale, contrast } => {
